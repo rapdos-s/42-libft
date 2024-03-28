7316568d38258b297718c793d98d9e15dd4b54e0
Raphael Dos santos esteves <rapdos-s@c1r5p4.42sp.org.br>
2024-03-27 22:04:49 -0300

    isprint tests done

--- tests/test_ft_isascii.c
+++ tests/test_ft_isascii.c
@@ -12,17 +12,18 @@
 
 #include <tests.h>
 
-static int	common_chars_test(void)
+static int	ascii_chars_test(void)
 {
-	char	*common_chars;
 	int		i;
 
 	i = 0;
-	common_chars = "012789ABCXYZabcxyz";
-	while (common_chars[i])
+	while (i <= 127)
 	{
-		if (ft_isascii(common_chars[i]) != FT_ISASCII_RETURN)
+		if (ft_isascii(i) != FT_ISASCII_RETURN)
+		{
+			printf(FAIL_ALERT "ft_isascii: Fail on ascii chars test\n");
 			return (0);
+		}
 		i++;
 	}
 	return (1);
@@ -38,21 +39,32 @@ static int	special_chars_test(void)
 	while (special_chars[i])
 	{
 		if (ft_isascii(special_chars[i]) != FT_ISASCII_RETURN)
+		{
+			printf(FAIL_ALERT "ft_isascii: Fail on special chars test\n");
 			return (0);
+		}
 		i++;
+	}
+	if (ft_isascii(0) != FT_ISASCII_RETURN)
+	{
+		printf(FAIL_ALERT "ft_isascii: Fail on special chars test\n");
+		return (0);
 	}
 	return (1);
 }
 
-static int	low_ascii_test(void)
+static int	non_ascii_chars_test(void)
 {
 	int		i;
 
-	i = 0;
-	while (i < 32)
+	i = 128;
+	while (i < 255)
 	{
-		if (ft_isascii(i) != FT_ISASCII_RETURN)
+		if (ft_isascii(i) != 0)
+		{
+			printf(FAIL_ALERT "ft_isascii: Fail on non ascii chars test\n");
 			return (0);
+		}
 		i++;
 	}
 	return (1);
@@ -66,7 +78,10 @@ static int	negative_chars_test(void)
 	while (i < 0)
 	{
 		if (ft_isascii(i) != 0)
+		{
+			printf(FAIL_ALERT "ft_isascii: Fail on negative chars test\n");
 			return (0);
+		}
 		i++;
 	}
 	return (1);
@@ -77,10 +92,9 @@ int	main(void)
 	int	result;
 
 	result = 0;
-	result += common_chars_test();
+	result += ascii_chars_test();
 	result += special_chars_test();
-	result += low_ascii_test();
+	result += non_ascii_chars_test();
 	result += negative_chars_test();
-	result = 0;
 	print_result(result, "ft_isascii");
 }
--- tests/test_ft_isprint.c
+++ tests/test_ft_isprint.c
@@ -14,15 +14,16 @@
 
 static int	printable_chars_test(void)
 {
-	char	*printable_chars;
 	int		i;
 
-	i = 0;
-	printable_chars = "012789ABCXYZabcxyz./ ";
-	while (printable_chars[i])
+	i = 32;
+	while (i <= 126)
 	{
-		if (ft_isprint(printable_chars[i]) != FT_ISPRINT_RETURN)
+		if (ft_isprint(i) != FT_ISPRINT_RETURN)
+		{
+			printf(FAIL_ALERT "ft_isprint: Fail on printable chars test\n");
 			return (0);
+		}
 		i++;
 	}
 	return (1);
@@ -38,7 +39,10 @@ static int	control_chars_test(void)
 	while (control_chars[i])
 	{
 		if (ft_isprint(control_chars[i]) != 0)
+		{
+			printf(FAIL_ALERT "ft_isprint: Fail on control chars test\n");
 			return (0);
+		}
 		i++;
 	}
 	return (1);
@@ -52,7 +56,20 @@ static int	non_printable_chars_test(void)
 	while (i < 32)
 	{
 		if (ft_isprint(i) != 0)
+		{
+			printf(FAIL_ALERT "ft_isprint: Fail on non printable chars test\n");
 			return (0);
+		}
+		i++;
+	}
+	i = 127;
+	while (i < 256)
+	{
+		if (ft_isprint(i) != 0)
+		{
+			printf(FAIL_ALERT "ft_isprint: Fail on non printable chars test\n");
+			return (0);
+		}
 		i++;
 	}
 	return (1);
@@ -62,11 +79,14 @@ static int	negative_chars_test(void)
 {
 	int		i;
 
-	i = -100;
+	i = -128;
 	while (i < 0)
 	{
 		if (ft_isprint(i) != 0)
+		{
+			printf(FAIL_ALERT "ft_isprint: Fail on negative chars test\n");
 			return (0);
+		}
 		i++;
 	}
 	return (1);
@@ -81,6 +101,5 @@ int	main(void)
 	result += control_chars_test();
 	result += non_printable_chars_test();
 	result += negative_chars_test();
-	result = 0;
 	print_result(result, "ft_isprint");
 }
