4f6100e23e33917781025563ba2c6afb96fc4d15
rapdos-s <raphael.santos.esteves@gmail.com>
2024-04-01 21:30:51 -0300

    ft_putnbr_fd tests done

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: rapdos-s <rapdos-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 19:29:26 by rapdos-s          #+#    #+#             */
-/*   Updated: 2024/03/31 13:52:09 by rapdos-s         ###   ########.fr       */
+/*   Updated: 2024/04/01 21:14:21 by rapdos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,27 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	(void) n;
-	(void) fd;
+	char	buffer[11];
+	int		i;
+	long	ln;
+
+	if (n == 0)
+		write(fd, "0", 1);
+	else
+	{
+		i = 11;
+		ln = n;
+		if (ln < 0)
+		{
+			write(fd, "-", 1);
+			ln = -ln;
+		}
+		while (ln != 0)
+		{
+			i--;
+			buffer[i] = ln % 10 + '0';
+			ln = ln / 10;
+		}
+	}
+	write(fd, buffer + i, 11 - i);
 }
--- tests/test_ft_putnbr_fd.c
+++ tests/test_ft_putnbr_fd.c
@@ -12,10 +12,148 @@
 
 #include <tests.h>
 
+static int	positive_numbers_tests(void)
+{
+	char	buffer[13];
+	int		buffer_fd[2];
+	int		i;
+	ssize_t	nbytes;
+
+	if (pipe(buffer_fd) == -1)
+	{
+		printf(ERROR_ALERT "ft_putnbr_fd: pipe failed, errno = %d\n", errno);
+		return (0);
+	}
+	ft_putnbr_fd(42, buffer_fd[1]);
+	ft_putnbr_fd(2147483647, buffer_fd[1]);
+	close(buffer_fd[1]);
+	nbytes = read(buffer_fd[0], buffer, sizeof(buffer));
+	if (nbytes == -1)
+	{
+		printf(ERROR_ALERT "ft_putnbr_fd: read failed, errno = %d\n", errno);
+		return (0);
+	}
+	i = 0;
+	while ("422147483647"[i])
+	{
+		if (buffer[i] != "422147483647"[i])
+		{
+			printf(FAIL_ALERT "ft_putnbr_fd: Fail on positive numbers test\n");
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+static int	negative_numbers_tests(void)
+{
+	char	buffer[15];
+	int		buffer_fd[2];
+	int		i;
+	ssize_t	nbytes;
+
+	if (pipe(buffer_fd) == -1)
+	{
+		printf(ERROR_ALERT "ft_putnbr_fd: pipe failed, errno = %d\n", errno);
+		return (0);
+	}
+	ft_putnbr_fd(-42, buffer_fd[1]);
+	ft_putnbr_fd(-2147483648, buffer_fd[1]);
+	close(buffer_fd[1]);
+	nbytes = read(buffer_fd[0], buffer, sizeof(buffer));
+	if (nbytes == -1)
+	{
+		printf(ERROR_ALERT "ft_putnbr_fd: read failed, errno = %d\n", errno);
+		return (0);
+	}
+	i = 0;
+	while ("-42-2147483648"[i])
+	{
+		if (buffer[i] != "-42-2147483648"[i])
+		{
+			printf(FAIL_ALERT "ft_putnbr_fd: Fail on negative numbers test\n");
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+static int	number_zero_tests(void)
+{
+	char	buffer[1];
+	int		buffer_fd[2];
+	ssize_t	nbytes;
+
+	if (pipe(buffer_fd) == -1)
+	{
+		printf(ERROR_ALERT "ft_putnbr_fd: pipe failed, errno = %d\n", errno);
+		return (0);
+	}
+	buffer[0] = '*';
+	ft_putnbr_fd(0, buffer_fd[1]);
+	close(buffer_fd[1]);
+	nbytes = read(buffer_fd[0], buffer, sizeof(buffer));
+	if (nbytes == -1)
+	{
+		printf(ERROR_ALERT "ft_putnbr_fd: read failed, errno = %d\n", errno);
+		return (0);
+	}
+	if (buffer[0] != '0')
+	{
+		printf(FAIL_ALERT "ft_putnbr_fd: Fail on number zero test\n");
+		return (0);
+	}
+	return (1);
+}
+
+static int	ten_multiple_tests(void)
+{
+	char	buffer[39];
+	int		buffer_fd[2];
+	int		i;
+	ssize_t	nbytes;
+
+	if (pipe(buffer_fd) == -1)
+	{
+		printf(ERROR_ALERT "ft_putnbr_fd: pipe failed, errno = %d\n", errno);
+		return (0);
+	}
+	ft_putnbr_fd(10, buffer_fd[1]);
+	ft_putnbr_fd(100000, buffer_fd[1]);
+	ft_putnbr_fd(1000000000, buffer_fd[1]);
+	ft_putnbr_fd(-10, buffer_fd[1]);
+	ft_putnbr_fd(-100000, buffer_fd[1]);
+	ft_putnbr_fd(-1000000000, buffer_fd[1]);
+	close(buffer_fd[1]);
+	nbytes = read(buffer_fd[0], buffer, sizeof(buffer));
+	if (nbytes == -1)
+	{
+		printf(ERROR_ALERT "ft_putnbr_fd: read failed, errno = %d\n", errno);
+		return (0);
+	}
+	i = 0;
+	while ("101000001000000000-10-100000-1000000000"[i])
+	{
+		if (buffer[i] != "101000001000000000-10-100000-1000000000"[i])
+		{
+			printf(FAIL_ALERT "ft_putnbr_fd: Fail on ten multiple test\n");
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
 int	main(void)
 {
 	int	result;
 
 	result = 0;
+	result += positive_numbers_tests();
+	result += negative_numbers_tests();
+	result += number_zero_tests();
+	result += ten_multiple_tests();
 	print_result(result, "ft_putnbr_fd");
 }
