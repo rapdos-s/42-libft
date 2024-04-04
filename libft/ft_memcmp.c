e84cd0dd7e08e13dd93b17a0f0cad8e3d0b08828
rapdos-s <raphael.santos.esteves@gmail.com>
2024-04-03 23:06:34 -0300

    ft_memcmp tests done

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: rapdos-s <rapdos-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 19:29:33 by rapdos-s          #+#    #+#             */
-/*   Updated: 2024/03/31 13:47:19 by rapdos-s         ###   ########.fr       */
+/*   Updated: 2024/04/03 18:32:23 by rapdos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,27 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	(void) s1;
-	(void) s2;
-	(void) n;
-	return (0);
+	size_t	i;
+	const unsigned char	*uc_s1;
+	const unsigned char	*uc_s2;
+
+	if (s1 == NULL && s2 == NULL)
+		return (0);	
+	uc_s2 = (const unsigned char *)s2;
+	if (s1 == NULL)
+		return (-(int)(uc_s2[0]));
+	uc_s1 = (const unsigned char *)s1;
+	if (s2 == NULL)
+		return ((int)(uc_s1[0]));
+	i = 0;
+	while (i < n && uc_s1[i] == uc_s2[i])
+		i++;
+	if (i == n)
+		return (0);
+	if (uc_s1[0] == '\0' || uc_s2[0] == '\0')
+		return (uc_s1[i] - uc_s2[i]);
+	else if (uc_s1[i] > uc_s2[i])
+		return (1);
+	else
+		return (-1);
 }
--- tests/test_ft_memcmp.c
+++ tests/test_ft_memcmp.c
@@ -12,10 +12,67 @@
 
 #include <tests.h>
 
+static int	common_cases_tests(void)
+{
+	if (ft_memcmp("ABC", "ABC", 3) != 0 || \
+		ft_memcmp("ABC", "123", 3) != 1 || \
+		ft_memcmp("123", "ABC", 3) != -1)
+	{
+		printf(FAIL_ALERT "ft_memcmp: Fail on common cases tests\n");
+		return (0);
+	}
+	return (1);
+}
+
+static int	short_size_tests(void)
+{
+	if (ft_memcmp("ABC", "AB", 2) != 0 || \
+		ft_memcmp("AB", "ABC", 2) != 0 || \
+		ft_memcmp("ABC", "AB", 0) != 0 || \
+		ft_memcmp("AB", "ABC", 0) != 0)
+	{
+		printf(FAIL_ALERT "ft_memcmp: Fail on short size tests\n");
+		printf("ft_memcmp(\"ABC\", \"AB\", 2) == %d\n", ft_memcmp("ABC", "AB", 2));
+		printf("ft_memcmp(\"AB\", \"ABC\", 2) == %d\n", ft_memcmp("AB", "ABC", 2));
+		printf("ft_memcmp(\"ABC\", \"AB\", 0) == %d\n", ft_memcmp("ABC", "AB", 0));
+		printf("ft_memcmp(\"AB\", \"ABC\", 0) == %d\n", ft_memcmp("AB", "ABC", 0));
+		return (0);
+	}
+	return (1);
+}
+
+static int	empty_string_tests(void)
+{
+	if (ft_memcmp("", "", 1) != 0 || \
+		ft_memcmp("", "ABC", 3) != -65 || \
+		ft_memcmp("ABC", "", 3) != 65)
+	{
+		printf(FAIL_ALERT "ft_memcmp: Fail on empty string tests\n");
+		return (0);
+	}
+	return (1);
+}
+
+static int	null_tests(void)
+{
+	if (ft_memcmp(NULL, "ABC", 10) != -65 || \
+		ft_memcmp("ABC", NULL, 10) != 65 || \
+		ft_memcmp(NULL, NULL, 10) != 0)
+	{
+		printf(FAIL_ALERT "ft_memcmp: Fail on NULL tests\n");
+		return (0);
+	}
+	return (1);
+}
+
 int	main(void)
 {
 	int	result;
 
 	result = 0;
+	result += common_cases_tests();
+	result += short_size_tests();
+	result += empty_string_tests();
+	result += null_tests();
 	print_result(result, "ft_memcmp");
 }
